3d37c7dca40594a04284e6263bd8d0bd2713ce5a
Ujin <tladbwls0430@gmail.com>
2025-10-14 10:40:25 +0900

    타입 import 방식 수정 (PropsWithChildren 오류 해결)

--- src/components/Reveal.tsx
+++ src/components/Reveal.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from "react";
+import type { PropsWithChildren } from "react";
 import { useInView } from "../hooks/useInView";
 
 type RevealProps = PropsWithChildren<{
